499100b07e061f50cbc7bba8e6eafefbdcb5537f
Android Studio <devandroid@ort>
2025-06-06 09:03:20 -0300

    inicio de HOME

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ import IniciarSesion from './source/Login/IniciarSesion';
 import Registrarse from './source/Login/Registrarse'
 import IniciarComoSolver from './source/Login/IniciarComoSolver'
 import Registrarse2 from './source/Login/Registrarse2'
-import index from './source/Home/index';
+import Home from './source/Home/index';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Registrarse2);
+registerRootComponent(Home);
