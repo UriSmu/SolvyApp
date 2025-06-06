dab810fda6b894e17f37578b2db759ecbed2d761
Android Studio <devandroid@ort>
2025-06-06 09:49:17 -0300

    avances en HOME

--- source/Home/index.js
+++ source/Home/index.js
@@ -1,138 +1,155 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
-import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { ImageBackground } from 'react-native-web';
+  import { StatusBar } from 'expo-status-bar';
+  import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native';
+  import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+  import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+  import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+  import { ImageBackground } from 'react-native-web';
+  import { LinearGradient } from 'expo-linear-gradient';
 
 
 
-export default function Home() {
-  const fondoServicios = require('../../assets/Fondo-servicios.png')
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar style="auto" />
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        {/* Encabezado */}
-        <View style={styles.header}>
-          <Text style={styles.logoText}>LOGO</Text>
-          <Text style={styles.iconoPerfil}>PERFIL</Text>
-        </View>
+  export default function Home() {
+    //const fondoServicios = require('../../assets/Fondo-servicios.png')
+    return (
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="auto" />
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          {/* Encabezado */}
+          <View style={styles.header}>
+            <Text style={styles.logoText}>LOGO</Text>
+            <Text style={styles.iconoPerfil}>PERFIL</Text>
+          </View>
 
-        {/* Saludo */}
-        <Text style={styles.titulo}>¡Hola, (NOMBRE)!</Text>
+          {/* Saludo */}
+          <Text style={styles.titulo}>¡Hola, (NOMBRE)!</Text>
 
-        {/* Servicios recientes */}
-        <TouchableOpacity style={styles.botonReciente}>
-          <Text style={styles.textoReciente}>(Servicios favoritos/recientes)</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botonReciente}>
-          <Text style={styles.textoReciente}>(Servicios favoritos/recientes)</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.botonReciente}>
-          <Text style={styles.textoReciente}>(Servicios favoritos/recientes)</Text>
-        </TouchableOpacity>
+          {/* Servicios recientes */}
+          <TouchableOpacity style={styles.botonReciente}>
+            <Text style={styles.textoReciente}>(Servicios favoritos/recientes)</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botonReciente}>
+            <Text style={styles.textoReciente}>(Servicios favoritos/recientes)</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.botonReciente}>
+            <Text style={styles.textoReciente}>(Servicios favoritos/recientes)</Text>
+          </TouchableOpacity>
 
-        {/* Título servicios recomendados */}
-        <Text style={styles.subtitulo}>Servicios recomendados</Text>
+          {/* Título servicios recomendados */}
+          <Text style={styles.subtitulo}>Servicios recomendados</Text>
 
-        {/* Servicios recomendados */}
-        <View style={styles.filaServicios}>
+          {/* Servicios recomendados */}
+          <View style={styles.filaServicios}>
           <View style={styles.servicio}>
-            <ImageBackground source={fondoServicios}>
-              <View style={styles.iconoServicio}><Text style={styles.iconoTexto}><FontAwesome6 name="fire-burner" size={50} color="white" /></Text></View>
-            </ImageBackground>
+            <LinearGradient colors={['#007cc0', '#003f5c']} style={styles.iconoServicio}>
+              <FontAwesome6 name="fire-burner" size={50} color="white" />
+            </LinearGradient>
             <Text style={styles.nombreServicio}>Gasista</Text>
           </View>
+
           <View style={styles.servicio}>
-            <View style={styles.iconoServicio}><Text style={styles.iconoTexto}><MaterialCommunityIcons name="chef-hat" size={50} color="white" /></Text></View>
+            <LinearGradient colors={['#007cc0', '#003f5c']} style={styles.iconoServicio}>
+              <MaterialCommunityIcons name="chef-hat" size={50} color="white" />
+            </LinearGradient>
             <Text style={styles.nombreServicio}>Cocina</Text>
           </View>
+
           <View style={styles.servicio}>
-            <View style={styles.iconoServicio}><Text style={styles.iconoTexto}><FontAwesome6 name="broom" size={50} color="white" /></Text></View>
+            <LinearGradient colors={['#007cc0', '#003f5c']} style={styles.iconoServicio}>
+              <FontAwesome6 name="broom" size={50} color="white" />
+            </LinearGradient>
             <Text style={styles.nombreServicio}>Limpieza</Text>
           </View>
         </View>
 
+
         <View style={styles.filaServicios}>
-          <View style={styles.servicio}>
-            <View style={styles.iconoServicio}><Text style={styles.iconoTexto}><MaterialIcons name="local-car-wash" size={50} color="white" /></Text></View>
-            <Text style={styles.nombreServicio}>Lavado de auto</Text>
-          </View>
-          <View style={styles.servicio}>
-            <View style={styles.iconoServicio}><Text style={styles.iconoTexto}><MaterialIcons name="electrical-services" size={50} color="white" /></Text></View>
-            <Text style={styles.nombreServicio}>Electricista</Text>
-          </View>
-          <View style={styles.servicio}>
-            <View style={styles.iconoServicio}><Text style={styles.iconoTexto}><FontAwesome6 name="truck-moving" size={50} color="white" /></Text></View>
-            <Text style={styles.nombreServicio}>Mudancero</Text>
-          </View>
+        <View style={styles.servicio}>
+          <LinearGradient colors={['#007cc0', '#003f5c']} style={styles.iconoServicio}>
+            <MaterialIcons name="local-car-wash" size={50} color="white" />
+          </LinearGradient>
+          <Text style={styles.nombreServicio}>Lavado de auto</Text>
         </View>
-      </ScrollView>
 
-      {/* Menú inferior */}
-      <View style={styles.menuInferior}>
-        <Text style={styles.menuItem}>🏠 Home</Text>
-        <Text style={styles.menuItem}>👷 Servicios</Text>
-        <Text style={styles.menuItem}>🛒 Productos</Text>
-        <Text style={styles.menuItem}>📊 Actividad</Text>
+        <View style={styles.servicio}>
+          <LinearGradient colors={['#007cc0', '#003f5c']} style={styles.iconoServicio}>
+            <MaterialIcons name="electrical-services" size={50} color="white" />
+          </LinearGradient>
+          <Text style={styles.nombreServicio}>Electricista</Text>
+        </View>
+
+        <View style={styles.servicio}>
+          <LinearGradient colors={['#007cc0', '#003f5c']} style={styles.iconoServicio}>
+            <FontAwesome6 name="truck-moving" size={50} color="white" />
+          </LinearGradient>
+          <Text style={styles.nombreServicio}>Mudancero</Text>
+        </View>
       </View>
-    </SafeAreaView>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff' },
-  scrollContainer: { padding: 20, paddingBottom: 120 },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  logoText: { fontSize: 18, fontWeight: 'bold' },
-  iconoPerfil: { fontSize: 24 },
-  titulo: { fontSize: 26, fontWeight: 'bold', marginVertical: 20 },
-  botonReciente: {
-    backgroundColor: '#007cc0',
-    padding: 12,
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-  textoReciente: { color: '#fff', fontStyle: 'italic', textAlign: 'center' },
-  subtitulo: { fontSize: 20, fontWeight: 'bold', marginTop: 20, marginBottom: 10, marginBottom: 30},
-  filaServicios: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 15,
+        </ScrollView>
+
+        {/* Menú inferior */}
+        <View style={styles.menuInferior}>
+          <Text style={styles.menuItem}> Home</Text>
+          <Text style={styles.menuItem}> Servicios</Text>
+          <Text style={styles.menuItem}> Productos</Text>
+          <Text style={styles.menuItem}> Actividad</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: { flex: 1, backgroundColor: '#fff',  },
+    scrollContainer: { padding: 20, paddingBottom: 120 },
+    header: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+    },
+    logoText: { fontSize: 18, fontWeight: 'bold' },
+    iconoPerfil: { fontSize: 24 },
+    titulo: { fontSize: 26, fontWeight: 'bold', marginVertical: 20 },
+    botonReciente: {
+      backgroundColor: '#007cc0',
+      padding: 12,
+      borderRadius: 10,
+      marginBottom: 20,
+    },
+    textoReciente: { color: '#fff', fontStyle: 'italic', textAlign: 'center' },
+    subtitulo: { fontSize: 30, fontWeight: 'bold', marginTop: 20, marginBottom: 10, marginBottom: 30, textAlign: 'center'},
+    filaServicios: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginBottom: 15,
+      
+    },
+    servicio: {
+      width: '30%',
+      alignItems: 'center',
+      
+    },
+    iconoServicio: {
+      backgroundColor: '#007cc0',
+      width: 95,
+      height: 95, 
+      borderRadius: 12,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 5,
+    },
     
-  },
-  servicio: {
-    width: '30%',
-    alignItems: 'center',
-    
-  },
-  iconoServicio: {
-    backgroundColor: '#007cc0',
-    width: 90,
-    height: 90, 
-    borderRadius: 12,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginBottom: 5,
-  },
-  
-  iconoTexto: { fontSize: 24, color: '#fff' },
-  nombreServicio: { textAlign: 'center', fontSize: 12 },
-  menuInferior: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    paddingVertical: 15,
-    backgroundColor: '#007cc0',
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    position: 'absolute',
-    bottom: 0,
-    width: '100%',
-  },
-  menuItem: { color: '#fff', fontWeight: 'bold' },
-});
+    iconoTexto: { fontSize: 24, color: '#fff' },
+    nombreServicio: { textAlign: 'center', fontSize: 12 },
+    menuInferior: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      paddingVertical: 15,
+      backgroundColor: '#007cc0',
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      position: 'absolute',
+      bottom: 0,
+      width: '100%',
+    },
+    menuItem: { color: '#fff', fontWeight: 'bold' },
+  });
