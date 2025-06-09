b2d5f768fb6f18f13b82a6f5227a7b6214db9fa5
Android Studio <devandroid@ort>
2025-06-09 08:18:01 -0300

    AuthContext

--- App.js
+++ App.js
@@ -1,47 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, SafeAreaView, Image, Button, TouchableOpacity, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Login from './source/Login'
+import { AuthProvider, useAuth } from './source/context/AuthContext';
+
+import Login from './source/Login';
 import IniciarComoCliente from './source/Login/IniciarComoCliente';
 import IniciarComoSolver from './source/Login/IniciarComoSolver';
 import IniciarSesion from './source/Login/IniciarSesion';
 import Registrarse from './source/Login/Registrarse';
 import Registrarse2 from './source/Login/Registrarse2';
 
-import Home from './source/Home'
+import Home from './source/Home';
 
+const Stack = createNativeStackNavigator();
 
-const Stack = createNativeStackNavigator()
-
-function LoginStack()
-{
-  return(
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Login" component={Login}/>
-      <Stack.Screen name="IniciarComoCliente" component={IniciarComoCliente}/>
-      <Stack.Screen name="IniciarComoSolver" component={IniciarComoSolver}/>
-      <Stack.Screen name="IniciarSesion" component={IniciarSesion}/>
-      <Stack.Screen name="Registrarse" component={Registrarse}/>
-      <Stack.Screen name="Registrarse2" component={Registrarse2}/>
+function LoginStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="IniciarComoCliente" component={IniciarComoCliente} />
+      <Stack.Screen name="IniciarComoSolver" component={IniciarComoSolver} />
+      <Stack.Screen name="IniciarSesion" component={IniciarSesion} />
+      <Stack.Screen name="Registrarse" component={Registrarse} />
+      <Stack.Screen name="Registrarse2" component={Registrarse2} />
     </Stack.Navigator>
-  )
+  );
 }
 
-function HomeStack(){
-  <Stack.Navigator screenOptions={{headerShown: false}}>
-    <Stack.Screen name="Home" component={Home}/>
-  </Stack.Navigator>
+function HomeStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+}
+
+function RootNavigation() {
+  const { estaLogeado } = useAuth();
+  return estaLogeado ? <HomeStack /> : <LoginStack />;
 }
 
 export default function App() {
-
-  const estaLogeado = false;
- 
   return (
-    <NavigationContainer>
-      {estaLogeado ? <HomeStack/> : <LoginStack/>}
-    </NavigationContainer>
-  )
+    <AuthProvider>
+      <NavigationContainer>
+        <RootNavigation />
+      </NavigationContainer>
+    </AuthProvider>
+  );
 }
