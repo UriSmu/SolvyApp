85ad99f50fc0a32a8ac7146bd444cb546c0d384b
Android Studio <devandroid@ort>
2025-06-23 08:31:38 -0300

    Traer arreglo

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ import ProductosScreen from './source/Home/productos';
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(ProductosScreen);
+registerRootComponent(App);
