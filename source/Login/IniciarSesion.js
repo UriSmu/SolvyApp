960fb0749934d7309ab6ac64393a799c61983755
UriSmu <urismu07@gmail.com>
2025-07-06 12:49:40 -0300

    Traer

--- source/Login/IniciarSesion.js
+++ source/Login/IniciarSesion.js
@@ -33,7 +33,7 @@ export default function IniciarSesion() {
     }
 
     try {
-      const response = await fetch(`http://localhost:3000/cli/clientes/${encodeURIComponent(usuario)}/${encodeURIComponent(contrasena)}`);
+      const response = await fetch(`https://solvy-app-api.vercel.app/cli/clientes/${encodeURIComponent(usuario)}/${encodeURIComponent(contrasena)}`);
 
       if (response.status === 404) {
         setLoginError('Usuario o contraseña incorrectos');
